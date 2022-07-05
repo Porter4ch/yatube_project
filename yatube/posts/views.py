9a1e8fc782d2439596698af51cc63668f3aac311
Шкарупин Андрей <Argon45645@gmail.com>
2022-07-05 13:06:25 +0500

    Запросов в urls.py - создал адрес для главной странице в yatube/urls.py - в приложении Posts создал файл urls.py - создал view-функции в views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
+from email.policy import HTTP
+import http
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HTTPResponse('Главная страница')
+
+def grup_posts(request):
+        return HTTPResponse('Группы по интересам')
